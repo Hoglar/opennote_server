30c00d9f062fb7591bf988cc475151a48564b012
Hoglar <martin.hoglar@gmail.com>
2022-05-24 13:47:07 +0200

    Update

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000
+const path = require("path");
+
+app.use(express.static("/home/hoglar/opennote_re/frontend"))
 
 app.get("/", (req, res) => {
-    res.send("Hello World from server");
+    console.log("Here")
 })
 
+
 app.get("/api", (req, res) => {
-    res.send("Hello world from api")
+    console.log(__dirname)
+    res.sendFile(path.join("/home/hoglar/opennote_re/frontend", "/index.html"));
 })
+
 
 app.listen(port, () => {
     console.log("Running at port")
